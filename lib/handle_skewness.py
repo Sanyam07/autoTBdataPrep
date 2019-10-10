dee465470927d8db6f0bfb35b2ff46668387a9c0
tawabshakeel <tawab2013@gmail.com>
2019-10-10 09:06:55 -0700

    handle negative zero values

--- lib/handle_skewness.py
+++ lib/handle_skewness.py
@@ -8,37 +8,29 @@ class HandleSkewness(object):
     def __init__(self):
         pass
 
-
-    def get_minimum_value(self,df):
+    def handle_neg_values(self, df):
         """
 
         :param df:
         :return:
         """
-        pass
+        min_value = df.agg({df.columns[0]: "min"}).collect()
+        if min_value <= 0:
+            df = df.withColumn(df.columns[0], funct.col(df.columns[0]) + abs(min_value) + 0.01)
+            return df
 
-    def handle_zero_values(self,df):
-        """
-
-        :param df:
-        :return:
-        """
-        pass
-
-    def handle_neg_values(self,df):
-        """
-
-        :param df:
-        :return:
-        """
-        pass
-
-    def remove_skewness(self,df,columns):
+    def remove_skewness(self, df, columns):
         """
 
         :param df:
         :param columns:
         :return:
         """
-
-
+        for col in columns:
+            skew_val = df.select(funct.skewness(df[col]))
+            if skew_val < 0:
+                self.handle_neg_values(df[col])
+            elif skew_val > 0:
+                pass
+            else:
+                return df
