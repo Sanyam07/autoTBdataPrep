a95ed0ee1cbbf5309a988827395c6749f3ee9016
tawabshakeel <tawab2013@gmail.com>
2019-10-19 13:00:23 +0500

    removed url duplication removal bug and added in pipeline
    Former-commit-id: 68f5d145d15a7d57e3024cdc938cb8a09cc93d7a

--- main/doodle_pipelines.py
+++ main/doodle_pipelines.py
@@ -1,15 +1,18 @@
 from lib_v2.columns_dropping_nan import ColumnsDroppingContainsNan
 from lib_v2.columns_dropping_with_same_value import ColumnsDroppingSameValue
+from lib_v2.removing_duplication_urls import RemovingDuplicationUrl
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from lib_v2.logs import logger, file_logs
 from pyspark.ml import Pipeline
+
 file_logs("mltrons")
 
 s = SparkContext.getOrCreate()
 
-
 sql = SparkSession(s)
+
+
 def columns_same_value():
     try:
         df = sql.read.csv("./run/column_rem.csv", inferSchema=True, header=True)
@@ -31,17 +34,37 @@ def remove_cols_containing_nan():
     try:
 
         logger.debug("this is debug")
-        df = sql.read.csv("./run/rem_test.csv", inferSchema=True, header=True)
+        df = sql.read.csv("./run/column_rem.csv", inferSchema=True, header=True)
 
-        return_df = Duplication().remove_columns_containing_all_nan_values(df)
-        return_df.toPandas().to_csv('./run/rem_test_result.csv')
+        col_contains_nan = ColumnsDroppingContainsNan()
+        model = Pipeline(stages=[col_contains_nan]).fit(df)
+        result = model.transform(df)
+
+        result.toPandas().to_csv('./run/pipeline_nan_value.csv')
         print(df.show())
         print("#####################")
         print("resulted_df")
-        print(return_df.show())
+        print(result.show())
+
     except Exception as e:
         logger.error(e)
 
 
-if __name__ =="__main__":
-    columns_same_value()
+def remove_url_duplication():
+    df = sql.read.csv("./run/date_test_res.csv", inferSchema=True, header=True)
+
+    url_duplication = RemovingDuplicationUrl()
+    model = Pipeline(stages=[url_duplication]).fit(df)
+    result = model.transform(df)
+
+    result.toPandas().to_csv('./run/pipeline_url.csv')
+
+    print("resulted_df")
+    print(result.show())
+
+
+
+if __name__ == "__main__":
+    # columns_same_value()
+    # remove_cols_containing_nan()
+    remove_url_duplication()
