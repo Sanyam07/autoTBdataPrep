b3a08bc233be3d54c5432a14efdca0fad5645a2e
tawabshakeel <tawab2013@gmail.com>
2019-10-19 21:51:06 +0500

    skewness code converted into pipeline structure
    Former-commit-id: 350d47a4de2b7bf85d16a25c3122b6278bb0364a

--- main/doodle_pipelines.py
+++ main/doodle_pipelines.py
@@ -1,6 +1,8 @@
 from lib_v2.columns_dropping_nan import ColumnsDroppingContainsNan
 from lib_v2.columns_dropping_with_same_value import ColumnsDroppingSameValue
 from lib_v2.removing_duplication_urls import RemovingDuplicationUrl
+from lib_v2.datetime_formatting import DatetimeFormatting
+from lib_v2.minimize_skewness import MinimizeSkewness
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from lib_v2.logs import logger, file_logs
@@ -63,8 +65,26 @@ def remove_url_duplication():
     print(result.show())
 
 
+def date_conversion():
+    df = sql.read.csv("./run/date_test_res.csv", inferSchema=True, header=True)
+    datetime_formatting = DatetimeFormatting()
+    model = Pipeline(stages=[datetime_formatting]).fit(df)
+    res = model.transform(df)
+    print("resulted_df")
+    print(res.show())
+
+
+def remove_skewness():
+    df = sql.read.csv("./run/file1.csv", inferSchema=True, header=True)
+    min_skewness = MinimizeSkewness(['Purpose'])
+    model  = Pipeline(stages=[min_skewness]).fit(df)
+    res = model.transform(df)
+    print(res.show())
+
 
 if __name__ == "__main__":
     # columns_same_value()
     # remove_cols_containing_nan()
-    remove_url_duplication()
+    # remove_url_duplication()
+    # date_conversion()
+    remove_skewness()
